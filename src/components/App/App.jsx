ed138204b2711176519d7c3306dd9395a187b39c
TurtlePower <agnkvdm@gmail.com>
2023-06-22 14:18:04 +0300

    refactoring from classes to hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,36 +1,42 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { FeedbackBtnList } from 'components/FeedbackBtnList/FeedbackBtnList';
 import { Container } from './App.styled';
 import { Rating } from 'components/Rating/Rating';
 import { Section } from 'components/Section/Section';
 import { Notification } from 'components/Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const currentRating = { good, neutral, bad };
+
+  const handleClick = ratingName => {
+    switch (ratingName) {
+      case 'good':
+        setGood(state => state + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(neutral => neutral + 1);
+        break;
+
+      case 'bad':
+        setBad(bad => bad + 1);
+        break;
+
+      default:
+        return;
+    }
   };
 
-  handleClick = ratingName => {
-    this.setState(prevState => {
-      return {
-        [ratingName]: prevState[ratingName] + 1,
-      };
-    });
+  const countTotalRating = () => {
+    return good + neutral + bad;
   };
 
-  countTotalRating = () => {
-    return Object.values(this.state).reduce(
-      (total, value) => (total += value),
-      0
-    );
-  };
-
-  countPositiveFeedback = () => {
-    const { good } = this.state;
-
-    const total = this.countTotalRating();
+  const countPositiveFeedback = () => {
+    const total = countTotalRating();
 
     if (total === 0) {
       return 0;
@@ -41,31 +47,29 @@ export class App extends Component {
     return Number(goodPercentage);
   };
 
-  render() {
-    const total = this.countTotalRating();
-    const goodRating = this.countPositiveFeedback();
+  const total = countTotalRating();
+  const goodRating = countPositiveFeedback();
 
-    return (
-      <Container>
-        <Section titleText="Please leave feedback">
-          <FeedbackBtnList
-            ratingList={Object.keys(this.state)}
-            onLeaveFeedback={this.handleClick}
+  return (
+    <Container>
+      <Section titleText="Please leave feedback">
+        <FeedbackBtnList
+          ratingList={Object.keys(currentRating)}
+          onLeaveFeedback={handleClick}
+        />
+      </Section>
+      <Section titleText="Statistics">
+        {total < 1 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Rating
+            ratingList={Object.keys(currentRating)}
+            currentRating={currentRating}
+            total={total}
+            positivePercentage={goodRating}
           />
-        </Section>
-        <Section titleText="Statistics">
-          {total < 1 ? (
-            <Notification message="There is no feedback" />
-          ) : (
-            <Rating
-              ratingList={Object.keys(this.state)}
-              currentRating={this.state}
-              total={total}
-              positivePercentage={goodRating}
-            />
-          )}
-        </Section>
-      </Container>
-    );
-  }
-}
+        )}
+      </Section>
+    </Container>
+  );
+};
